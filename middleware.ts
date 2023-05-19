bb908312b95d231c59af273fa30f58eb5399863b
Joey <info@therun.gg>
2023-05-19 23:22:02 +0200

    readded middleware

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const reroutes = [
+        "/WaifuRuns",
+        "/gsa",
+        "/dirtythirty",
+        "/saesr",
+        "/saesr_events",
+    ];
+
+    function shouldReroute(reroute: string, pathname: string): boolean {
+        return (
+            reroute.toLowerCase() === pathname.toLowerCase() &&
+            reroute !== pathname
+        );
+    }
+
+    const redirect = reroutes.find((reroute) =>
+        shouldReroute(reroute, request.nextUrl.pathname)
+    );
+
+    if (redirect) {
+        return NextResponse.redirect(
+            `${request.nextUrl.origin}${redirect}${request.nextUrl.search}`
+        );
+    }
+
+    return NextResponse.next();
+}
