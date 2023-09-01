7c3ce00ce105a21137657fa2f01a30fde565ef14
therun.gg <info@therun.gg>
2023-09-01 16:18:32 +0200

    added support for multiple game/categories in tournaments and fixed game search (#118)

--- src/lib/live-runs.ts
+++ src/lib/live-runs.ts
@@ -1,4 +1,6 @@
 import { safeEncodeURI } from "~src/utils/uri";
+import { Tournament } from "~src/components/tournament/tournament-info";
+import { LiveRun } from "~app/live/live.types";
 
 export const liveRunUrl =
     "https://pokzhwoycl3uo7n5lzh6iltyoq0iaibq.lambda-url.eu-west-1.on.aws/";
@@ -18,10 +20,18 @@ export const getAllLiveRuns = async (game = null, category = null) => {
     return result.json();
 };
 
+export const getLiveRunsForTournament = async (tournament: Tournament) => {
+    const results = tournament.eligibleRuns.map((run) =>
+        getLiveRunsForGameCategory(run.game, run.category)
+    );
+
+    return (await Promise.all(results)).flat();
+};
+
 export const getLiveRunsForGameCategory = async (
     game: string,
     category: string
-) => {
+): Promise<LiveRun[]> => {
     const result = await fetch(
         `${liveRunUrl}?game=${safeEncodeURI(game)}&category=${safeEncodeURI(
             category
