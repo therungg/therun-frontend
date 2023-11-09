390cb420ba03d6bc837ab051953d8486cd0b6a40
therun.gg <info@therun.gg>
2023-11-09 15:19:33 +0100

    pace-120 (#138)

--- app/tournaments/tournament-list.ts
+++ app/tournaments/tournament-list.ts
@@ -17,7 +17,7 @@ const tournamentEntries: Array<[string, TournamentEntryValue]> = [
         { tournament: "Defeat Ganon No SRM Community Clash Main Event" },
     ],
     ["dirtythirty", { tournament: "Dirty Thirty Sapphire Tourney 2" }],
-    ["gsa", { tournament: "PACE 16" }],
+    ["gsa", { tournament: "PACE 120" }],
     ["hgss", { tournament: "HGSS Blitz" }],
     ["lego", { tournament: "Lego Challenge 3" }],
     [
