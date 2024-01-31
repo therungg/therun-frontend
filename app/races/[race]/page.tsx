05df190596257c958b3ac0bf4b1b507894f2d08c
Joey <info@therun.gg>
2024-01-31 01:46:21 +0100

    removed unused metadata

--- app/races/[race]/page.tsx
+++ app/races/[race]/page.tsx
@@ -1,7 +1,6 @@
 import { getRaceByRaceId } from "~src/lib/races";
 import { RaceDetail } from "~app/races/[race]/race-view";
 import { getSession } from "~src/actions/session.action";
-import buildMetadata from "~src/utils/metadata";
 
 interface PageProps {
     params: { race: string };
@@ -13,9 +12,3 @@ export default async function RaceDetailPage({ params }: PageProps) {
 
     return <RaceDetail race={race} user={session} />;
 }
-
-export const metadata = buildMetadata({
-    title: "Watch Live Runs",
-    description:
-        "Watch streams of runners who are currently live and attempting a run, and discover new runners for your favorite games!",
-});
