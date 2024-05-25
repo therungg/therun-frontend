71a5e7b786a0af0d6db95eeb09c35677b5a11254
Flo Wolfe <florencewolfe@pm.me>
2024-05-25 19:11:15 -0400

    feat: add pseudo-ErrorBoundary for session errors (#237)
    * feat: add pseudo-errorboundary for session errors
    
    * Add reset session button to header
    
    * Update src/components/topbar.tsx
    
    Co-authored-by: Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>

--- src/lib/get-session-data.ts
+++ src/lib/get-session-data.ts
@@ -1,10 +1,25 @@
+import { SessionError } from "~src/common/session.error";
+
 export const getSessionData = async (sessionId: string): Promise<unknown> => {
     if (!sessionId) {
         return {};
     }
+
     const url = `https://6ob8kz9k4g.execute-api.eu-west-1.amazonaws.com/session?id=${sessionId}&returnUser=true`;
 
-    return (
-        await (await fetch(url, { next: { revalidate: 60 * 60 * 2 } })).json()
-    )?.result?.data;
+    try {
+        const response = await fetch(url, {
+            next: { revalidate: 60 * 60 * 2 },
+        });
+        const result = await response.json();
+        const session = result?.result?.data;
+
+        if (!session) {
+            throw new SessionError("Session not found");
+        }
+
+        return session;
+    } catch (error) {
+        throw new SessionError("An error occurred recovering session data");
+    }
 };
