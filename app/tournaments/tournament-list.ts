e73bbc2526ba93d1f42a2fe21533b803f484ba11
Joey <info@therun.gg>
2024-12-10 22:24:50 +0100

    kakariko

--- app/tournaments/tournament-list.ts
+++ app/tournaments/tournament-list.ts
@@ -70,6 +70,7 @@ const tournamentEntries: Array<[string, TournamentEntryValue]> = [
         },
     ],
     ["rayman", { tournament: "Rayman 1 BTR" }],
+    ["kakarikoclassic2024", { tournament: "Kakariko Classic 2024" }],
 ];
 
 const tournamentMap: Map<string, Tournament | CombinedTournament> = new Map(
