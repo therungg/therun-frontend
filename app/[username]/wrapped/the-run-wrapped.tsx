3a9f1641ab05f73df0c7a33f8a7964e4df822e7f
Flo Wolfe <florencewolfe@pm.me>
2024-12-30 17:15:22 -0500

    add page counter

--- app/[username]/wrapped/the-run-wrapped.tsx
+++ app/[username]/wrapped/the-run-wrapped.tsx
@@ -1,8 +1,9 @@
+import { useMemo, useRef, useState } from "react";
 import { WrappedWithData } from "~app/[username]/wrapped/wrapped-types";
 import { WrappedTitle } from "~app/[username]/wrapped/wrapped-title";
 import { ScrollDown } from "~src/components/scroll-down";
-import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { WrappedStreak } from "./sections/wrapped-streak";
 import { WrappedSocialImages } from "./sections/wrapped-social-images";
@@ -10,6 +11,7 @@ import { WrappedTopGames } from "./sections/wrapped-top-games";
 import { WrappedTotalStatsCompliment } from "./sections/wrapped-total-stats-compliment";
 import { WrappedActivityGraphs } from "~app/[username]/wrapped/sections/wrapped-activity-graphs";
 
+gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 interface TheRunWrappedProps {
@@ -18,59 +20,70 @@ interface TheRunWrappedProps {
 }
 
 export const TheRunWrapped = ({ wrapped, user }: TheRunWrappedProps) => {
+    const [sectionIndex, setSectionIndex] = useState(-1);
     const containerRef = useRef(null);
+    // https://gsap.com/resources/React/
+    useGSAP(
+        () => {
+            if (!wrapped.hasEnoughRuns) return;
 
-    useEffect(() => {
-        if (!wrapped.hasEnoughRuns) return;
+            const sections: gsap.DOMTarget[] =
+                gsap.utils.toArray(".animated-section");
 
-        const sections: gsap.DOMTarget[] =
-            gsap.utils.toArray(".animated-section");
-
-        ScrollTrigger.defaults({
-            //markers: true,
-            pin: true,
-            scrub: 0.5,
-        });
-
-        sections.forEach((section, _) => {
-            ScrollTrigger.create({
-                trigger: section,
+            ScrollTrigger.defaults({
+                pin: true,
+                scrub: 0.5,
             });
-        });
 
-        return () => {
-            ScrollTrigger.getAll().forEach((st) => st.kill());
-        };
-    }, [wrapped.hasEnoughRuns]);
-
-    return (
-        <div ref={containerRef}>
-            <section className="flex-center flex-column min-vh-100-no-header mesh-bg text-center">
-                <p className="display-2 mb-0">You had a great 2024!</p>
-                <WrappedTitle user={user} />
-                <p className="display-6 mb-5">
-                    Let's see your stats for this year!
-                </p>
-                <ScrollDown />
-            </section>
-
-            <section className="animated-section text-center flex-center align-items-center min-vh-100">
+            sections.forEach((section, index) => {
+                console.log({ section, index });
+                ScrollTrigger.create({
+                    trigger: section,
+                    onEnter: () => setSectionIndex(index),
+                    onLeaveBack: () => setSectionIndex(index - 1),
+                });
+            });
+        },
+        {
+            dependencies: [wrapped.hasEnoughRuns],
+            scope: containerRef,
+            revertOnUpdate: true,
+        },
+    );
+    const sections = useMemo(() => {
+        return [
+            <section
+                key="wrapped-total-stats-compliment"
+                className="animated-section text-center flex-center align-items-center min-vh-100"
+            >
                 <WrappedTotalStatsCompliment wrapped={wrapped} />
-            </section>
+            </section>,
 
-            <section className="animated-section text-center flex-center align-items-center min-vh-100">
+            <section
+                key="wrapped-activity-graphs"
+                className="animated-section text-center flex-center align-items-center min-vh-100"
+            >
                 <WrappedActivityGraphs wrapped={wrapped} />
-            </section>
+            </section>,
 
-            <section className="animated-section flex-center flex-column align-items-center min-vh-100">
+            <section
+                key="wrapped-streak"
+                className="animated-section flex-center flex-column align-items-center min-vh-100"
+            >
                 <WrappedStreak wrapped={wrapped} />
-            </section>
+            </section>,
 
-            <section className="animated-section text-center min-vh-100">
+            <section
+                key="wrapped-top-games"
+                className="animated-section text-center min-vh-100"
+            >
                 <WrappedTopGames wrapped={wrapped} />
-            </section>
+            </section>,
 
-            <section className="animated-section flex-center flex-column align-items-center min-vh-100">
+            <section
+                key="wrapped-outro"
+                className="animated-section flex-center flex-column align-items-center min-vh-100"
+            >
                 <p className="text-center flex-center align-items-center display-2 mb-4">
                     That's a wrap on 2024!
                 </p>
@@ -79,7 +92,33 @@ export const TheRunWrapped = ({ wrapped, user }: TheRunWrappedProps) => {
                     what you do in 2025!
                 </p>
                 <WrappedSocialImages wrapped={wrapped} />
+            </section>,
+        ];
+    }, [wrapped]);
+
+    return (
+        <div ref={containerRef}>
+            <section
+                key="wrapped-intro"
+                className="flex-center flex-column min-vh-100-no-header mesh-bg text-center"
+            >
+                <p className="display-2 mb-0">You had a great 2024!</p>
+                <WrappedTitle user={user} />
+                <p className="display-6 mb-5">
+                    Let's see your stats for this year!
+                </p>
+                <ScrollDown />
             </section>
+            {sections}
+            {sectionIndex + 1 === 0 ||
+            sectionIndex + 1 === sections.length ? null : (
+                <h2
+                    style={{ bottom: 25, opacity: 0.66 }}
+                    className="sticky-bottom text-end"
+                >
+                    {sectionIndex + 1} / {sections.length - 1}
+                </h2>
+            )}
         </div>
     );
 };
