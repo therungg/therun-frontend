222e3acb6fa35eed5d0c33275dd70dde5a523af3
Flo Wolfe <florencewolfe@pm.me>
2024-12-31 14:58:56 -0500

    add flex to the overview

--- app/[username]/wrapped/sections/wrapped-stats-overview.tsx
+++ app/[username]/wrapped/sections/wrapped-stats-overview.tsx
@@ -15,117 +15,122 @@ export const WrappedStatsOverview = memo<WrappedStatsOverviewProps>(
             }, 0);
         }, [wrapped.pbsAndGolds]);
         return (
-            <div>
-                <Row>
+            <div className="h-100">
+                <Row className="mt-5">
                     <h2>Stats Overview</h2>
                 </Row>
-                <Row className="mb-5">
-                    <Col>
-                        <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
-                            <span className="display-1 fw-semibold text-decoration-underline">
-                                <WrappedCounter
-                                    id="total-runs-count"
-                                    end={wrapped.totalRuns}
-                                />
-                            </span>
-                        </div>
-                        <div className="flex-center h4">
-                            <div>
-                                This year, you started a total of{" "}
-                                <b>{wrapped.totalRuns}</b> runs!
+                <div className="h-100 d-flex flex-column justify-content-center ">
+                    <Row className="mb-5">
+                        <Col>
+                            <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
+                                <span className="display-1 fw-semibold text-decoration-underline">
+                                    <WrappedCounter
+                                        id="total-runs-count"
+                                        end={wrapped.totalRuns}
+                                    />
+                                </span>
                             </div>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
-                            <span className="display-1 fw-semibold text-decoration-underline">
-                                <WrappedCounter
-                                    id="total-finished-runs-count"
-                                    end={wrapped.totalFinishedRuns}
-                                />
-                            </span>
-                        </div>
-                        <div className="flex-center h4">
-                            <div>
-                                Of these <b>{wrapped.totalRuns}</b> runs, you
-                                finished <b>{wrapped.totalFinishedRuns}</b>
+                            <div className="flex-center h4">
+                                <div>
+                                    This year, you started a total of{" "}
+                                    <b>{wrapped.totalRuns}</b> runs!
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
-                            <span className="display-1 fw-semibold text-decoration-underline">
-                                <WrappedCounter
-                                    id="total-finished-runs-percentage"
-                                    end={
-                                        (wrapped.totalFinishedRuns /
-                                            wrapped.totalRuns) *
-                                        100 *
-                                        100
-                                    }
-                                    duration={2}
-                                    formattingFn={(value: number) => {
-                                        return (value / 100).toFixed(2) + "%";
-                                    }}
-                                />
-                            </span>
-                        </div>
-                        <div className="flex-center h4">
-                            <div>
-                                That gives you a finish percentage of{" "}
-                                <b>
-                                    {" "}
-                                    {(
-                                        (wrapped.totalFinishedRuns /
-                                            wrapped.totalRuns) *
-                                        100
-                                    ).toFixed(2)}
-                                    %
-                                </b>
+                        </Col>
+                        <Col>
+                            <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
+                                <span className="display-1 fw-semibold text-decoration-underline">
+                                    <WrappedCounter
+                                        id="total-finished-runs-count"
+                                        end={wrapped.totalFinishedRuns}
+                                    />
+                                </span>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
-                <Row className="mb-5">
-                    <Col>
-                        <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
-                            <span className="display-1 fw-semibold text-decoration-underline">
-                                <WrappedCounter
-                                    id="total-golds-count"
-                                    end={wrapped.totalGolds}
-                                    style={{
-                                        background:
-                                            "linear-gradient(to right, #d19e1d, #ffd86e, #e3a812)",
-                                        color: "transparent",
-                                        backgroundClip: "text",
-                                    }}
-                                />
-                            </span>
-                        </div>
-                        <div className="flex-center h4">
-                            <div>
-                                Wow! You got <b>{wrapped.totalGolds}</b> gold
-                                splits this year!
+                            <div className="flex-center h4">
+                                <div>
+                                    Of these <b>{wrapped.totalRuns}</b> runs,
+                                    you finished{" "}
+                                    <b>{wrapped.totalFinishedRuns}</b>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
-                            <span className="display-1 fw-semibold text-decoration-underline">
-                                <WrappedCounter
-                                    id="total-pbs-count"
-                                    end={personalBestCount}
-                                />
-                            </span>
-                        </div>
-                        <div className="flex-center h4">
-                            <div>
-                                You managed to get {personalBestCount} personal
-                                bests!
+                        </Col>
+                        <Col>
+                            <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
+                                <span className="display-1 fw-semibold text-decoration-underline">
+                                    <WrappedCounter
+                                        id="total-finished-runs-percentage"
+                                        end={
+                                            (wrapped.totalFinishedRuns /
+                                                wrapped.totalRuns) *
+                                            100 *
+                                            100
+                                        }
+                                        duration={2}
+                                        formattingFn={(value: number) => {
+                                            return (
+                                                (value / 100).toFixed(2) + "%"
+                                            );
+                                        }}
+                                    />
+                                </span>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
+                            <div className="flex-center h4">
+                                <div>
+                                    That gives you a finish percentage of{" "}
+                                    <b>
+                                        {" "}
+                                        {(
+                                            (wrapped.totalFinishedRuns /
+                                                wrapped.totalRuns) *
+                                            100
+                                        ).toFixed(2)}
+                                        %
+                                    </b>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className="mb-5">
+                        <Col>
+                            <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
+                                <span className="display-1 fw-semibold text-decoration-underline">
+                                    <WrappedCounter
+                                        id="total-golds-count"
+                                        end={wrapped.totalGolds}
+                                        style={{
+                                            background:
+                                                "linear-gradient(to right, #d19e1d, #ffd86e, #e3a812)",
+                                            color: "transparent",
+                                            backgroundClip: "text",
+                                        }}
+                                    />
+                                </span>
+                            </div>
+                            <div className="flex-center h4">
+                                <div>
+                                    Wow! You got <b>{wrapped.totalGolds}</b>{" "}
+                                    gold splits this year!
+                                </div>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className="flex-center bg-body-secondary mb-3 game-border border-secondary px-4 py-5 rounded-3">
+                                <span className="display-1 fw-semibold text-decoration-underline">
+                                    <WrappedCounter
+                                        id="total-pbs-count"
+                                        end={personalBestCount}
+                                    />
+                                </span>
+                            </div>
+                            <div className="flex-center h4">
+                                <div>
+                                    You managed to get {personalBestCount}{" "}
+                                    personal bests!
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         );
     },
