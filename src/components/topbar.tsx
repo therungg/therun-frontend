80a3c7c7fd14bcdbe3924c788fb3a11d12b88dd0
Flo Wolfe <florencewolfe@pm.me>
2025-01-07 19:32:29 -0500

    update topbar for wrapped

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,5 +1,15 @@
 "use client";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {
+    Alert,
+    AlertHeading,
+    Badge,
+    Button,
+    Container,
+    Nav,
+    Navbar,
+    NavDropdown,
+    Stack,
+} from "react-bootstrap";
 import dynamic from "next/dynamic";
 import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -74,113 +84,139 @@ const Topbar = ({ username, picture, sessionError }: Partial<TopbarProps>) => {
     }
 
     return (
-        <Navbar
-            expand="lg"
-            onMouseLeave={hideDropdown}
-            data-bs-theme={dark ? "dark" : "light"}
-        >
-            <Container>
-                <Navbar.Brand href="/" className="d-flex">
-                    <Image
-                        alt="TheRun"
-                        src={`/logo_${
-                            dark ? "dark" : "light"
-                        }_theme_no_text_transparent.png`}
-                        height="44"
-                        width="44"
-                        className="img-fluid align-self-start me-2"
-                    />
-                    <span className="align-self-center">
-                        The Run{" "}
-                        <i>
-                            <sup>beta</sup>
-                        </i>
-                    </span>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {username && (
-                            <Nav.Link className="mh-2r" href="/upload">
-                                <div className="d-flex">
-                                    <b>Upload</b>
+        <>
+            <Navbar
+                expand="lg"
+                onMouseLeave={hideDropdown}
+                data-bs-theme={dark ? "dark" : "light"}
+            >
+                <Container>
+                    <Navbar.Brand href="/" className="d-flex">
+                        <Image
+                            alt="TheRun"
+                            src={`/logo_${
+                                dark ? "dark" : "light"
+                            }_theme_no_text_transparent.png`}
+                            height="44"
+                            width="44"
+                            className="img-fluid align-self-start me-2"
+                        />
+                        <span className="align-self-center">
+                            The Run{" "}
+                            <i>
+                                <sup>beta</sup>
+                            </i>
+                        </span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {username && (
+                                <Nav.Link className="mh-2r" href="/upload">
+                                    <div className="d-flex">
+                                        <b>Upload</b>
 
-                                    <span className="ms-2">
-                                        <Upload size={18} />
-                                    </span>
-                                </div>
+                                        <span className="ms-2">
+                                            <Upload size={18} />
+                                        </span>
+                                    </div>
+                                </Nav.Link>
+                            )}
+                            <Can I="view-restricted" a="stories">
+                                <Nav.Link href="/stories/manage">
+                                    Story Mode
+                                </Nav.Link>
+                            </Can>
+                            <Nav.Link href={`/wrapped/${username}`}>
+                                Wrapped 2024
+                                <i className="text-secondary">
+                                    <sup>New</sup>
+                                </i>
                             </Nav.Link>
-                        )}
-                        <Can I="view-restricted" a="stories">
-                            <Nav.Link href="/stories/manage">
-                                Story Mode
-                            </Nav.Link>
-                        </Can>
-                        <Nav.Link href="/races">Races</Nav.Link>
-                        <Nav.Link href="/tournaments">Tournaments</Nav.Link>
-                        <Nav.Link href="/live">Live</Nav.Link>
-                        <Nav.Link href="/games/">Games</Nav.Link>
-                    </Nav>
-                    <Nav className="ml-auto mb-2 mb-lg-0 me-lg-2">
-                        <GlobalSearch />
-                    </Nav>
-                    <Nav className="ml-auto" onClick={handleColorMode}>
-                        {" "}
-                        <DarkModeSlider />
-                    </Nav>
-                    <Nav
-                        className="ml-auto"
-                        onMouseEnter={showDropdown}
-                        onClick={showDropdown}
-                    >
-                        {username && (
-                            <NavDropdown
-                                show={show}
-                                title={
-                                    <TwitchUser
-                                        username={username}
-                                        picture={picture || ""}
-                                    />
-                                }
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href={`/${username}`}>
-                                    Profile
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/livesplit">
-                                    LiveSplit Key
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/change-appearance">
-                                    Name Appearance
-                                </NavDropdown.Item>
-                                <Can I="view-restricted" a="stories">
-                                    <NavDropdown.Item href="/stories/manage">
-                                        Story Preferences
-                                    </NavDropdown.Item>
-                                </Can>
-                                <NavDropdown.Item onClick={() => logout()}>
-                                    Logout
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        )}
-                        {sessionError && (
-                            <div className="ms-2">
-                                <button
-                                    className="btn btn-primary"
-                                    type="button"
-                                    onClick={handleResetSession}
+                            <Nav.Link href="/races">Races</Nav.Link>
+                            <Nav.Link href="/tournaments">Tournaments</Nav.Link>
+                            <Nav.Link href="/live">Live</Nav.Link>
+                            <Nav.Link href="/games/">Games</Nav.Link>
+                        </Nav>
+                        <Nav className="ml-auto mb-2 mb-lg-0 me-lg-2">
+                            <GlobalSearch />
+                        </Nav>
+                        <Nav className="ml-auto" onClick={handleColorMode}>
+                            {" "}
+                            <DarkModeSlider />
+                        </Nav>
+                        <Nav
+                            className="ml-auto"
+                            onMouseEnter={showDropdown}
+                            onClick={showDropdown}
+                        >
+                            {username && (
+                                <NavDropdown
+                                    show={show}
+                                    title={
+                                        <TwitchUser
+                                            username={username}
+                                            picture={picture || ""}
+                                        />
+                                    }
+                                    id="basic-nav-dropdown"
                                 >
-                                    Reset session
-                                </button>
-                            </div>
-                        )}
-                        {!sessionError && !username && (
-                            <TwitchLoginButton url="/api" />
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                                    <NavDropdown.Item href={`/${username}`}>
+                                        Profile
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="/livesplit">
+                                        LiveSplit Key
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="/change-appearance">
+                                        Name Appearance
+                                    </NavDropdown.Item>
+                                    <Can I="view-restricted" a="stories">
+                                        <NavDropdown.Item href="/stories/manage">
+                                            Story Preferences
+                                        </NavDropdown.Item>
+                                    </Can>
+                                    <NavDropdown.Item onClick={() => logout()}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            )}
+                            {sessionError && (
+                                <div className="ms-2">
+                                    <button
+                                        className="btn btn-primary"
+                                        type="button"
+                                        onClick={handleResetSession}
+                                    >
+                                        Reset session
+                                    </button>
+                                </div>
+                            )}
+                            {!sessionError && !username && (
+                                <TwitchLoginButton url="/api" />
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Alert className="container w-100">
+                <AlertHeading>
+                    <Badge>NEW</Badge>&nbsp;The Run Wrapped 2024 is here!!
+                </AlertHeading>
+                <Stack
+                    direction="horizontal"
+                    className="justify-content-between"
+                >
+                    <p>
+                        After several weeks of hard work, we're finally here!
+                        Wrapped brings you all of your 2024 speedrun stats
+                        summarized in a nice package.
+                    </p>
+                </Stack>
+                <div className="d-flex justify-content-center">
+                    <Button className="text-nowrap">Wrapped 2024</Button>
+                </div>
+            </Alert>
+        </>
     );
 };
 export default Topbar;
