ecf4498bca2cf403c9dc920e66b9f0e0782b183e
Devin Rowan <its@autumnrowan.com>
2025-01-28 14:43:18 -0600

    Make errors prettier, add CTA for Discord

--- app/error.tsx
+++ app/error.tsx
@@ -10,21 +10,41 @@ export default function Error({
     reset: () => void;
 }) {
     React.useEffect(() => {
-        // Log the error to an error reporting service
+        // TODO: Log the error to an error reporting service
         console.error(error);
     }, [error]);
 
     return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button
-                onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                }
-            >
-                Try again
-            </button>
+        <div className="d-flex flex-column align-items-center justify-content-center py-5">
+            <div className="text-center">
+                <h2 className="display-5 mb-3 text-danger fw-medium">
+                    Something went wrong!
+                </h2>
+                <p className="lead mb-4">
+                    Sorry about that. Let's try that again.
+                </p>
+                <button
+                    className="btn btn-primary px-4 py-2"
+                    onClick={
+                        // Attempt to recover by trying to re-render the segment
+                        () => reset()
+                    }
+                >
+                    Try again
+                </button>
+                <h3 className="mt-5 mb-3">Does this keep happening?</h3>
+                <p className="lead mb-4">
+                    You can let us know in our{" "}
+                    <a
+                        href="https://discord.gg/KuAwwPsKUp"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Discord
+                    </a>
+                    .
+                </p>
+            </div>
         </div>
     );
 }
