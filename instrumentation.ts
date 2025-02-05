b3f554f38ef7c352f3f2f54c8834f34a49b47477
Joey <info@therun.gg>
2025-02-06 00:43:31 +0100

    fix drizzle migration config

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,6 +2,6 @@ export async function register() {
     if (process.env.NEXT_RUNTIME === "nodejs") {
         const { migrate } = await import("drizzle-orm/node-postgres/migrator");
         const { db } = await import("./src/db/index");
-        await migrate(db, { migrationsFolder: "../drizzle" });
+        await migrate(db, { migrationsFolder: "./drizzle/" });
     }
 }
