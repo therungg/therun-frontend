bd20ea96b8bdd215388da66344c0963d887f64a4
Flo Wolfe <florencewolfe@pm.me>
2025-02-07 18:46:42 -0500

    Move libs to /lib/server to be explicit (#318)
    * Move libs to /lib/server to be explicit
    
    * add comments to make the migration easier later

--- src/lib/server/get-advanced-user-stats.ts
+++ src/lib/server/get-advanced-user-stats.ts
@@ -1,4 +1,5 @@
-export const advancedUserStats = async (user: string, timezone: string) => {
+"use server";
+export const getAdvancedUserStats = async (user: string, timezone: string) => {
     const res = await fetch(
         `https://dxg3hfz4b6ekynso45amgq2ife0tzhnl.lambda-url.eu-west-1.on.aws/${user}/${timezone}`,
     );
@@ -6,5 +7,3 @@ export const advancedUserStats = async (user: string, timezone: string) => {
 
     return json.result;
 };
-
-export default advancedUserStats;
