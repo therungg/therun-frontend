fe13bda0c06e35c96cb1dd6072836c46573aafe3
therun.gg <info@therun.gg>
2025-02-13 22:55:45 +0100

    implement basics of drizzle (#325)
    * implement basics of drizzle
    
    * we do NOT need the meta dir
    
    * we should ignore that
    
    * poc that it works
    
    * fix drizzle migration config
    
    * turns out we have to commit this lol
    
    * get events page poc
    
    * add seeding function
    
    * add seeding function
    
    * added db setup to readme
    
    * added db setup to readme

---
+++ drizzle/0002_outgoing_wither.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "events" ALTER COLUMN "location" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "events" ALTER COLUMN "bluesky" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "events" ALTER COLUMN "discord" DROP NOT NULL;
---
+++ src/db/schema.ts
@@ -0,0 +1,29 @@
+import {
+    integer,
+    pgTable,
+    text,
+    timestamp,
+    index,
+    varchar,
+} from "drizzle-orm/pg-core";
+
+export const events = pgTable(
+    "events",
+    {
+        id: integer().primaryKey().generatedAlwaysAsIdentity(),
+        startsAt: timestamp().notNull(),
+        endsAt: timestamp().notNull(),
+        name: varchar({ length: 255 }).notNull(),
+        type: varchar({ length: 255 }).notNull(),
+        location: varchar({ length: 255 }),
+        bluesky: varchar({ length: 255 }),
+        discord: varchar({ length: 255 }),
+        language: varchar({ length: 255 }).notNull(),
+        shortDescription: varchar({ length: 255 }).notNull(),
+        description: text().notNull(),
+        url: varchar({ length: 255 }),
+        imageUrl: varchar({ length: 255 }),
+        createdAt: timestamp().defaultNow(),
+    },
+    (table) => [index("starts_at_index").on(table.startsAt)],
+);
