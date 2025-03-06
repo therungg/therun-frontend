b2b54261f6ebc7b64d366e533394507177a0179f
Joey <info@therun.gg>
2025-03-06 22:50:42 +0100

    add value to types

--- src/types/summary.types.ts
+++ src/types/summary.types.ts
@@ -1,7 +1,9 @@
 export interface UserSummary {
     user: string;
     type: UserSummaryType;
-    "type#value": string;
+    // start date of the week of this summary when type === 'week', always monday (2025-03-03)
+    // start date of the month of this summary when type === 'month', always first of month (2025-03-01)
+    value: string;
     lastUpdate: number;
     totalRuns: number;
     totalFinishedRuns: number;
