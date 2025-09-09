bdff67353f50aec55682e728f64196ab5f2c85d1
Joey <info@therun.gg>
2025-09-10 01:10:41 +0200

    fix patron login

--- src/actions/user-patreon-data.action.ts
+++ src/actions/user-patreon-data.action.ts
@@ -6,6 +6,7 @@ import { safeEncodeURI } from "~src/utils/uri";
 import type { UserPatreonData } from "~app/change-appearance/patreon-section";
 
 const patreonApiBaseUrl = process.env.NEXT_PUBLIC_PATREON_API_URL;
+const patreonLoginUrl = process.env.NEXT_PUBLIC_PATREON_LOGIN_URL;
 
 export const getUserPatreonData = async (query: {
     code?: string;
@@ -24,16 +25,14 @@ export const getUserPatreonData = async (query: {
         const sessionId = session.id;
 
         const base = safeEncodeURI(`${baseUrl}/change-appearance`);
-        const loginUrl = `${patreonApiBaseUrl}?code=${code}&redirect_uri=${base}&session_id=${sessionId}`;
+        const loginUrl = `${patreonLoginUrl}?code=${code}&redirect_uri=${base}&session_id=${sessionId}`;
 
         const patreonLinkData = await fetch(loginUrl);
 
         return patreonLinkData.json();
     } else if (session.username) {
         const patreonDataUrl = `${patreonApiBaseUrl}/patreon/${session.username}`;
-        const patreonLinkData = await fetch(patreonDataUrl, {
-            next: { revalidate: 60 * 60 * 2 },
-        });
+        const patreonLinkData = await fetch(patreonDataUrl);
 
         return patreonLinkData.json();
     }
