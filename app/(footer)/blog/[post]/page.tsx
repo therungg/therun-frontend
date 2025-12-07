1968cc8fa689468bf3eb6b7cd6220555bb3e4f8e
Joey <info@therun.gg>
2025-12-08 00:41:13 +0100

    actually use cache

--- app/(footer)/blog/[post]/page.tsx
+++ app/(footer)/blog/[post]/page.tsx
@@ -1,15 +1,11 @@
-import { Post } from "./post";
+import { Post } from './post';
 
 const posts = [
-    "welcome-to-the-run",
-    "twitch-extension",
-    "the-run-live",
-    "the-run-racing",
+    'welcome-to-the-run',
+    'twitch-extension',
+    'the-run-live',
+    'the-run-racing',
 ];
-
-export function generateStaticParams() {
-    return posts.map((post) => ({ post }));
-}
 
 export default async function PostPage(props: {
     params: Promise<{ post: string }>;
