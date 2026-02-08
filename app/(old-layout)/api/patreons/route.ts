1226d16d3922f7fbcf762fc8f0e82ab39a9b0594
Joey <info@therun.gg>
2026-02-09 00:20:22 +0100

    remove swr

--- app/(old-layout)/api/patreons/route.ts
+++ app/(old-layout)/api/patreons/route.ts
@@ -1,4 +1,3 @@
-import { cacheLife } from 'next/cache';
 import { apiResponse } from '../response';
 import { getAllPatrons } from './get-all-patrons.action';
 
@@ -6,9 +5,5 @@ export async function GET() {
     const result = await getAllPatrons();
     return apiResponse({
         body: result,
-        cache: {
-            maxAge: 12000,
-            swr: 12000,
-        },
     });
 }
