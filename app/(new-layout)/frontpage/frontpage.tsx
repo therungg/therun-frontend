a86be3e8b40d3e7b1bb7b6c0698258ccb0ff1d29
Joey <info@therun.gg>
2026-02-23 22:20:52 +0100

    layout: move PB feed above community pulse in right column

--- app/(new-layout)/frontpage/frontpage.tsx
+++ app/(new-layout)/frontpage/frontpage.tsx
@@ -23,11 +23,11 @@ export default async function FrontPage() {
                 </Col>
                 <Col lg={5} xs={12}>
                     <div className="d-flex flex-column gap-4">
-                        <Suspense fallback={<SectionSkeleton height={300} />}>
-                            <CommunityPulse />
-                        </Suspense>
                         <Suspense fallback={<SectionSkeleton height={400} />}>
                             <PbFeedSection />
+                        </Suspense>
+                        <Suspense fallback={<SectionSkeleton height={300} />}>
+                            <CommunityPulse />
                         </Suspense>
                     </div>
                 </Col>
