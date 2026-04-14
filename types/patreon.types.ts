4a437ac1ed66e6fbaeba6b45eec76164256be92b
Joey <info@therun.gg>
2026-04-15 00:27:47 +0200

    feat(patron): extend PatronPreferences type for custom styling

--- types/patreon.types.ts
+++ types/patreon.types.ts
@@ -1,9 +1,38 @@
+export interface PerMode<T> {
+    dark: T;
+    light: T;
+}
+
+export interface TextShadowSpec {
+    color: string;
+    blur: number;
+}
+
+export interface OutlineSpec {
+    color: string;
+    width: number;
+}
+
 export interface PatronPreferences {
     hide: boolean;
-    featureOnOverview: boolean;
-    colorPreference: number;
     featureInScrollbar: boolean;
+    featureOnOverview: boolean;
     showIcon: boolean;
+
+    /** @deprecated Legacy preset id. Read for rendering; never written by new UI. */
+    colorPreference?: number;
+
+    customColor?: PerMode<string> | null;
+    customGradient?: PerMode<string[]> | null;
+
+    bold?: boolean;
+    italic?: boolean;
+
+    textShadow?: PerMode<TextShadowSpec> | null;
+    outline?: PerMode<OutlineSpec> | null;
+
+    gradientAngle?: PerMode<number> | null;
+    gradientAnimated?: boolean;
 }
 
 export interface Patron {
