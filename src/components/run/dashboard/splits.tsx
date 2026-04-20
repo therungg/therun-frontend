261fea0b4083c35fd59dabda02234ac8e890eecb
Joey <info@therun.gg>
2026-04-20 10:48:59 +0200

    refactor(run): remove inline splits download icon (moved to Downloads tab)

--- src/components/run/dashboard/splits.tsx
+++ src/components/run/dashboard/splits.tsx
@@ -1,9 +1,7 @@
 import { ValueOf } from 'next/dist/shared/lib/constants';
-import { useTheme } from 'next-themes';
 import React, { useState } from 'react';
 import { Col, Row, Table } from 'react-bootstrap';
 import Switch from 'react-switch';
-import { Bounce, ToastContainer, toast } from 'react-toastify';
 import { Run, SplitsHistory, SplitTimes } from '../../../common/types';
 import styles from '../../css/User.module.scss';
 import SplitName from '../../transformers/split-name';
@@ -37,74 +35,12 @@ export const Splits = ({ splits, gameTime = false, run }: SplitsProps) => {
     );
 
     const splitToUse = totalTime ? 'total' : 'single';
-    const splitsFile = decodeURIComponent(run.splitsFile as string)
-        .replaceAll('%', '%25')
-        .replaceAll('+++', '+%2B+')
-        .replaceAll('++', '%2B+')
-        .replaceAll('NG+', 'NG%2B');
-
-    const url = `${process.env.NEXT_PUBLIC_SPLITS_CLOUDFRONT_URL}/${splitsFile}`;
-    const fallbackUrl = `${process.env.NEXT_PUBLIC_SPLITS_CLOUDFRONT_URL}/${splitsFile.replaceAll('+', '%2B')}`;
-    const downloadFilename = `${run.user}_${run.game}_${run.run}.lss`;
-
-    const handleDownload = async () => {
-        toast.info(
-            `If you want to remove the run history on these splits, use 'Edit Splits' -> 'Other...' -> 'Clear History' from within LiveSplit.`,
-        );
-
-        let response = await fetch(url);
-        if (!response.ok) {
-            response = await fetch(fallbackUrl);
-        }
-
-        if (!response.ok) {
-            toast.error('Failed to download splits file.');
-            return;
-        }
-
-        const blob = new Blob([await response.blob()], {
-            type: 'application/octet-stream',
-        });
-        const blobUrl = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = blobUrl;
-        a.download = downloadFilename;
-        a.click();
-        URL.revokeObjectURL(blobUrl);
-    };
-
-    const theme = useTheme();
 
     return (
         <div>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                draggable
-                pauseOnHover
-                transition={Bounce}
-                theme={theme.theme || 'dark'}
-            />
             <Row>
                 <Col xl={9} style={{ whiteSpace: 'nowrap', display: 'flex' }}>
                     <h2>Splits {gameTime && '(IGT)'}</h2>
-                    {run.splitsFile && (
-                        <a
-                            rel="noreferrer"
-                            style={{ marginLeft: '0.5rem' }}
-                            href={url}
-                            download={downloadFilename}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                handleDownload();
-                            }}
-                        >
-                            <DownloadIcon />
-                        </a>
-                    )}
                 </Col>
                 <Col sm={3} style={{ display: 'flex', justifyContent: 'end' }}>
                     <div className="d-flex justify-content-start align-items-center justify-content-lg-center">
@@ -317,22 +253,5 @@ const TimeCell: React.FunctionComponent<TimeCellProps> = ({
                 </small>
             </div>
         </td>
-    );
-};
-
-// TODO: Move this to icons directory
-const DownloadIcon = () => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            className="bi bi-cloud-download"
-            viewBox="0 0 16 16"
-        >
-            <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
-            <path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z" />
-        </svg>
     );
 };
